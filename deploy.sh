746ddbfbf2302a33bf684e5368a8f5aba5282de4
Zefram Lou <zeframlou@gmail.com>
2018-12-11 22:04:18 -0800

    fixed deployment

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 coffee -c .
-browserify ./performance.js -o ./performance-bundle.js
+browserify ./performance.js -o ./performance-bundle.js -t [ babelify --presets [ @babel/preset-env @babel/preset-react ] ]
 cp ./performance-bundle.js ../Betoken.github.io/js
 cp ./init.js ../Betoken.github.io/js
